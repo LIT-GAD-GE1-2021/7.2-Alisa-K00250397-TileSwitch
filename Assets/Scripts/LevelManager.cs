4a9d8c986f17e41237a168e3a169ffd5e6243b1d
Alisa Gavrilova <64360518+GAD-Alisa@users.noreply.github.com>
2021-02-28 16:18:30 +0000

    Initial commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * The LevelManager from the Week 6 Lecture
+ */
+public class LevelManager : MonoBehaviour {
+
+    public static LevelManager instance;
+
+    public GameObject tileMapA;
+    public GameObject tileMapB;
+    public GameObject tileMapC;
+
+    private void Awake()
+    {
+        // set the instance property/variable to this object
+        instance = this;
+
+        tileMapB.SetActive(false);
+        tileMapC.SetActive(true);
+
+    }
+
+    public void SwitchTileMap()
+    { 
+
+        if (tileMapB.activeSelf == true)
+        {
+            tileMapB.SetActive(false);
+        }
+        else
+        { 
+            tileMapB.SetActive(true); 
+        }
+
+        if (tileMapC.activeSelf == true)
+        {
+            tileMapC.SetActive(false);
+        }
+        else
+        { 
+            tileMapC.SetActive(true); 
+        }
+
+    }
+
+    
+}
